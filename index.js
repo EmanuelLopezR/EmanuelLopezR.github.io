47cf7f15fa97eadf8894ff08fe0476c4cd9edbde
EmanuelLopezR <89672950+EmanuelLopezR@users.noreply.github.com>
2023-05-03 21:56:35 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function hello() {
+    alert("Toque de Queda es un programa enfocado a la música underground.");
+  }
+
+function Facebook() {
+    
+    window.open("https://www.facebook.com/ToqueDeQuedaFresno");
+    
+  }
+  
+  function showtime() {
+    var now = new Date();
+    alert(now);
+  }
+  
